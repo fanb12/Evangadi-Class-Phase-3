9d3b28bdfb1a1b6663592590e2ba04aa0375ebbe
fanb12 <129201755+fanb12@users.noreply.github.com>
2024-01-05 15:41:45 +0300

    Update App.jsx

--- React-functinal-componnent/apple-responsive-replica-html-css-mar-2020/react-apple/src/App.jsx
+++ React-functinal-componnent/apple-responsive-replica-html-css-mar-2020/react-apple/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react' 
+import { useState } from 'react'  
 import Header from "./componnent/Header"
 import Section1 from "./componnent/Section1"
 import Section2 from "./componnent/Section2"
